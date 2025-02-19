b3722069edb3e6c04dd4877c96e7a802c7599db8
Thesoulkid <133838463+Thesoulkid@users.noreply.github.com>
2025-02-19 14:00:25 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,9 @@
+module.exports = {
+    owner: "+639516894933",
+    botName: "RMS - V1",
+    ownerName: "SoulZinMaker",
+    privateMode: false, // Set true for private mode
+    authorizedUsers: [+639516894933], // Users who have access to the bot
+    premiumUsers: [+639516894933], // Users with premium access
+    userBots: {} // Stores user bots with pairing codes
+};
